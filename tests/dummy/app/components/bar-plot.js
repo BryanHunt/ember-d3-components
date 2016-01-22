dbc0c9554cafebac98c5ec10668d7b5b389b04f1
Bryan Hunt <bhunt@mac.com>
2016-01-22 09:12:23 -0600

    Add ordinal scale

--- tests/dummy/app/components/bar-plot.js
+++ tests/dummy/app/components/bar-plot.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import BarPlotter from '../utils/d3-bar-plotter';
+import OrdinalScale from '../utils/d3-scale-ordinal';
 import LinearScale from '../utils/d3-scale-linear';
 
 const { Component, observer } = Ember;
@@ -15,15 +16,11 @@ export default Component.extend({
   yGrid: Ember.Object.create({x1: 0, y1: 0, x2: 450, y2: 0}),
 
   plotter: BarPlotter.create({
-    xScale: LinearScale.create({domain: [0, 3], range: [0, 450]}),
+    xScale: OrdinalScale.create({domain: [0, 1, 2], rangeBands: [0, 450]}),
     yScale: LinearScale.create({domain: [100, 0], range: [0, 450]}),
     data: [{x: 0, y: 10}, {x: 1, y: 20}, {x: 2, y:35}],
     width: 450,
     height: 450
-  }),
-
-  updateXScale: observer('xMax', function() {
-    this.set('plotter.xScale.domain', [0, this.get('xMax')]);
   }),
 
   updateYScale: observer('yMax', function() {
