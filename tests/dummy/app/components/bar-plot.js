9a726d0183581744e7150db671cadfdb51df5605
Bryan Hunt <bhunt@mac.com>
2016-01-22 21:34:40 -0600

    Give bar plot a more meaningful x-axis

--- tests/dummy/app/components/bar-plot.js
+++ tests/dummy/app/components/bar-plot.js
@@ -16,9 +16,9 @@ export default Component.extend({
   yGrid: Ember.Object.create({x1: 0, y1: 0, x2: 450, y2: 0}),
 
   plotter: BarPlotter.create({
-    xScale: OrdinalScale.create({domain: [0, 1, 2], rangeBands: [0, 450]}),
+    xScale: OrdinalScale.create({domain: ["Apples", "Oranges", "Bananas"], rangeBands: [0, 450]}),
     yScale: LinearScale.create({domain: [100, 0], range: [0, 450]}),
-    data: [{x: 0, y: 10}, {x: 1, y: 20}, {x: 2, y:35}],
+    data: [{x: "Apples", y: 10}, {x: "Oranges", y: 20}, {x: "Bananas", y:35}],
     width: 450,
     height: 450
   }),
