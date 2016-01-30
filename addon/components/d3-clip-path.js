ad02ebbe147f42e9e861de8ca5710ef9e4dc291d
Bryan Hunt <bhunt@mac.com>
2016-01-29 20:27:30 -0600

    Code cleanup

--- addon/components/d3-clip-path.js
+++ addon/components/d3-clip-path.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { Component, computed } = Ember;
+const { Component } = Ember;
 
 export default Component.extend({
   tagName: 'clipPath',
--- addon/components/d3-grid.js
+++ addon/components/d3-grid.js
@@ -7,18 +7,28 @@ export default Axis.extend({
   init() {
     this._super.apply(this, arguments);
     this.set('tickFormat', "");
-    this.set('translateX', 25);
   },
 
   updateTransform: on('init', observer('width', 'height', 'orientation', function() {
+    Ember.run.once(this, this.updateGrid);
+  })),
+
+  updateGrid() {
     let orientation = this.get('orientation');
+    let translateY = 0;
+    let yOffset = this.get('yOffset');
+
+    if(yOffset !== undefined) {
+      translateY += yOffset
+    }
 
     if(orientation === 'top' || orientation === 'bottom') {
-      this.set('translateY', this.get('height') + 25);
+      translateY += this.get('height');
+      this.set('translateY', translateY);
       this.set('innerTickSize', -this.get('height'));
     } else {
-      this.set('translateY', 25);
+      this.set('translateY', translateY);
       this.set('innerTickSize', -this.get('width'));
     }
-  }))
+  }
 });
