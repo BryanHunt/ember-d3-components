1d58a2ba18f9ddf9b02fe7ac55b5a0c267fc51a0
Bryan Hunt <bhunt@mac.com>
2016-03-07 20:07:06 -0600

    Add data-accessor unit tests

--- tests/unit/utils/d3-data-accessor-test.js
+++ tests/unit/utils/d3-data-accessor-test.js
@@ -1,10 +1,24 @@
-import accessor from 'dummy/utils/d3-data-accessor';
+import Accessor from 'dummy/utils/d3-data-accessor';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | d3-data-accessor');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = accessor.create();
-  assert.ok(result);
+test('default name', function(assert) {
+  let accessor = Accessor.create();
+  let data = { value: 10 };
+  assert.equal(accessor.extract(data), 10);
+});
+
+test('specified name', function(assert) {
+  let accessor = Accessor.create({name: "y"});
+  let data = { y: 10 };
+  assert.equal(accessor.extract(data), 10);
+});
+
+test('custom extractor', function(assert) {
+  let accessor = Accessor.create({extract: function(dataPoint) {
+    return dataPoint[this.get('name')] + 1;
+  }});
+  let data = { value: 10 };
+  assert.equal(accessor.extract(data), 11);
 });
