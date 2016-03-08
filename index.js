b9d5cde4f6f2ebe7c604374e386671f8d34d8c55
Bryan Hunt <bhunt@mac.com>
2016-03-07 20:12:15 -0600

    Trying to fix for travis build

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = {
   name: 'ember-d3-components',
 
-  included(app) {
+  included: function(app) {
     this._super.included(app);
     app.import(app.bowerDirectory + '/d3/d3.js');
   }
