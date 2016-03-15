55020e5cd88fd7b55637556fd9a59bb36c64887a
Bryan Hunt <bhunt@mac.com>
2016-03-14 21:17:47 -0500

    Add real unit tests

--- tests/unit/utils/scales/d3-threshold-scale-test.js
+++ tests/unit/utils/scales/d3-threshold-scale-test.js
@@ -1,10 +1,37 @@
-import scalesD3ThresholdScale from 'dummy/utils/scales/d3-threshold-scale';
-import { module, test } from 'qunit';
+import Ember from 'ember';
+import { moduleFor, test } from 'ember-qunit';
 
-module('Unit | Utility | scales/d3 threshold scale');
+moduleFor('util:scales/d3-threshold-scale', 'Unit | Utility | scales/d3 threshold scale');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = scalesD3ThresholdScale.create();
-  assert.ok(result);
+test('domain/range', function(assert) {
+  let scale = this.subject({domain: [0, 10], range: ['a', 'b', 'c']}).get('scale');
+  assert.equal(scale(5), 'b');
+});
+
+test('domain element changed', function(assert) {
+  let domain = Ember.A([0, 10]);
+  let scale = this.subject({domain: domain, range: ['a', 'b', 'c']}).get('scale');
+  domain.replace(1, 1, 100);
+  assert.equal(scale(50), 'b');
+});
+
+test('domain changed', function(assert) {
+  let scaleObserver = this.subject({domain: [0, 10], range: ['a', 'b', 'c']});
+  let scale = scaleObserver.get('scale');
+  scaleObserver.set('domain', [0, 100]);
+  assert.equal(scale(50), 'b');
+});
+
+test('range element changed', function(assert) {
+  let range = Ember.A(['a', 'b', 'c']);
+  let scale = this.subject({domain: [0,10], range: range}).get('scale');
+  range.replace(1, 1, 'x');
+  assert.equal(scale(5), 'x');
+});
+
+test('range changed', function(assert) {
+  let scaleObserver = this.subject({domain: [0, 10], range: ['a', 'b', 'c']});
+  let scale = scaleObserver.get('scale');
+  scaleObserver.set('range', ['a', 'x', 'c']);
+  assert.equal(scale(5), 'x');
 });
