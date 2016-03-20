a6a0a21a29658f020b4aaab85555382f25198af1
Bryan Hunt <bhunt@mac.com>
2016-03-20 11:59:46 -0500

    Use property updater

--- addon/utils/layouts/d3-pie-layout.js
+++ addon/utils/layouts/d3-pie-layout.js
@@ -1,54 +1,30 @@
 import Ember from 'ember';
+import PropertyUpdater from '../../mixins/d3-property-updater';
 
 const { on, observer } = Ember;
 
-export default Ember.Object.extend({
+export default Ember.Object.extend(PropertyUpdater, {
   init() {
     this.set('layout', d3.layout.pie());
   },
 
   valueChanged: on('init', observer('value', function() {
-    let value = this.get('value');
-
-    if(value) {
-      this.get('layout').value(value);
-      Ember.run.next(this, 'notifyPropertyChange', 'layout');
-    }
+    this.updateProperty('layout', 'value');
   })),
 
   sortChanged: on('init', observer('sort', function() {
-    let sort = this.get('sort');
-
-    if(sort) {
-      this.get('layout').sort(sort);
-      Ember.run.next(this, 'notifyPropertyChange', 'layout');
-    }
+    this.updateProperty('layout', 'sort');
   })),
 
   startAngleChanged: on('init', observer('startAngle', function() {
-    let startAngle = this.get('startAngle');
-
-    if(startAngle) {
-      this.get('layout').startAngle(startAngle);
-      Ember.run.next(this, 'notifyPropertyChange', 'layout');
-    }
+    this.updateProperty('layout', 'startAngle');
   })),
 
   endAngleChanged: on('init', observer('endAngle', function() {
-    let endAngle = this.get('endAngle');
-
-    if(endAngle) {
-      this.get('layout').endAngle(endAngle);
-      Ember.run.next(this, 'notifyPropertyChange', 'layout');
-    }
+    this.updateProperty('layout', 'endAngle');
   })),
 
   padAngleChanged: on('init', observer('padAngle', function() {
-    let padAngle = this.get('padAngle');
-
-    if(padAngle) {
-      this.get('layout').padAngle(padAngle);
-      Ember.run.next(this, 'notifyPropertyChange', 'layout');
-    }
+    this.updateProperty('layout', 'padAngle');
   })),
 });
