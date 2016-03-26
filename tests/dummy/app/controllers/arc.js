bd32849e16ee2205844f7dc54a0feea3a22bb332
Bryan Hunt <bhunt@mac.com>
2016-03-26 00:19:49 -0500

    Fix unit tests

--- tests/dummy/app/controllers/arc.js
+++ tests/dummy/app/controllers/arc.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import LinearScale from '../utils/scales/d3-linear-scale';
 
 export default Ember.Controller.extend({
   data: [{startAngle: 0, endAngle: 3.14}]
--- tests/dummy/app/controllers/text.js
+++ tests/dummy/app/controllers/text.js
@@ -1,4 +1,3 @@
 import Ember from 'ember';
-import LinearScale from '../utils/scales/d3-linear-scale';
 
 export default Ember.Controller.extend({});
